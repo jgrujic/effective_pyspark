164356d85ab1b82e1463f3bd6ad93577ca99c06e
Oliver Willekens <4733178+oliverw1@users.noreply.github.com>
2024-03-15 13:50:34 +0100

    Improve module docstring, showing what's the intent

--- tests/test_distance_metrics.py
+++ tests/test_distance_metrics.py
@@ -1,6 +1,10 @@
 """
 Exercise: implement a series of tests with which you validate the
 correctness (or lack thereof) of the function great_circle_distance.
+
+The idea behind this exercise is to get you familiar with testing, so that when
+you develop code, you can quickly iterate and check that what you're developing
+matches the expectations.
 """
 
 from exercises.b_unit_test_demo.distance_metrics import great_circle_distance
